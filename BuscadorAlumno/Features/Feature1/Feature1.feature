7139e4d7109ccecb98703ce64b26a74b93257992
Nacho <joseignacio.rodriguez@tajamar365.com>
2016-05-05 12:26:07 +0000

    no message

---
+++ BuscadorAlumno/Features/Feature1/Feature1.feature
@@ -0,0 +1,13 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="6efbf485-2d52-42ed-a06c-f8cf05b0bbef" description="SharePoint Add-in Feature" featureId="6efbf485-2d52-42ed-a06c-f8cf05b0bbef" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="BuscadorAlumno Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="7fa7c982-a047-4a02-8dac-22c8a17d6daf" />
+    <projectItemReference itemId="9a3eedab-edb2-44fb-9970-1055dda87d89" />
+    <projectItemReference itemId="50ff48a6-17d7-4b0f-8e72-4051d80ecc69" />
+    <projectItemReference itemId="64df1316-57c1-42c8-be84-9d120bea75df" />
+    <projectItemReference itemId="4a87dc62-3dd7-4329-9b88-27f3c485144c" />
+    <projectItemReference itemId="fd3024b5-f379-46ca-8085-d3aa691d6aae" />
+    <projectItemReference itemId="328ad7e7-c4b1-4266-9d47-9732a7af5aeb" />
+    <projectItemReference itemId="6dba19b4-35bd-4b0f-90d5-76152ed89dd9" />
+  </projectItems>
+</feature>
---
+++ BuscadorAlumno/pkg/Debug/BuscadorAlumno/BuscadorAlumno_Feature1/Scripts/App.js
@@ -0,0 +1,83 @@
+﻿'use strict';
+
+$(function () {
+
+    $("#btnCursos").click(getAlumnos);
+
+});
+
+function getAlumnos() {
+
+    var duracion = $("#txtDuracion").val();
+
+    $("#displayDiv").empty();
+
+    $("#displayDiv").append($("<img>", { src: "_layouts/images/gears_an.gif" }));
+
+    var requestUri = _spPageContextInfo.webAbsoluteUrl + "/_api/lists/getbytitle('Curso')/items?$filter=duracion gt " + duracion;
+
+    $.ajax({
+
+        type: "GET",
+
+        url: requestUri,
+
+        contentType: "application/json",
+
+        headers: {
+
+            "accept": "application/json;odata=verbose",
+
+        },
+
+        success: onReturnData,
+
+        error: function (xhr, ajaxOptions, thrownError) {
+
+            alert(xhr.status);
+
+        }
+
+    });
+
+}
+
+function onReturnData(data) {
+
+    $("#displayDiv").empty();
+
+    var oDataResult = data.d.results;
+
+    var tableHeader = "<thead>" +
+
+    "<td>Nombre</td>" +
+
+    "<td>Duracion</td>" +
+
+    "<td>Inicio</td>" +
+
+    "<td>Fin</td>" +
+
+    "</thead>";
+
+    var table = $("<table>", { id: "alumnosTable" }).append($(tableHeader));
+
+    $.each(oDataResult, function (i, item) {
+
+        var row = "<tr>" +
+
+        "<td>" + item.nombre + "</td>" +
+
+        "<td>" + item.duracion + "</td>" +
+
+        "<td>" + item.inicio + "</td>" +
+
+        "<td>" + item.fin + "</td></tr>";
+
+        table.append(row);
+
+    });
+
+    $("#displayDiv").append(table);
+
+}
